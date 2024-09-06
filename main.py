3b5a27ecd3583561a8770288fa60cf1d4e9c295d
Shaleen Rawat <116910466+smcoder5@users.noreply.github.com>
2024-09-07 00:22:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as s
 import stellar_classify as sc
+import obesity_classify as oc
 import titanic as td
 
 
@@ -7,10 +8,12 @@ import titanic as td
 s.set_page_config(page_title='Kaggle Data Sets',layout='wide')
 with s.sidebar:
     
-    rad=s.radio("Ocean Data Set Page",['Stellar Classification','Titanic Dataset']) 
+    rad=s.radio(" Machine Learning Miscellanious",['Stellar Classification','Obesity Classification','Titanic Dataset']) 
 
 if(rad=='Stellar Classification'):
     sc.main()
 
 elif(rad=='Titanic Dataset'):
-    td.main()
+    td.main() 
+elif(rad=='Obesity Classification'):
+    oc.classify()
