41d839ca1112f90e4ea8db26279befbd0373661c
Shaleen Rawat <116910466+smcoder5@users.noreply.github.com>
2024-09-07 17:39:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import streamlit as s
 import stellar_classify as sc
 import obesity_classify as oc
 import titanic as td
+import flood_probability as fp
 
 
 
 s.set_page_config(page_title='Kaggle Data Sets',layout='wide')
 with s.sidebar:
     
-    rad=s.radio(" Machine Learning Miscellanious",['Stellar Classification','Obesity Classification','Titanic Dataset']) 
+    rad=s.radio(" Machine Learning Miscellanious",['Stellar Classification','Obesity Classification','Flood Probability','Titanic Dataset']) 
 
 if(rad=='Stellar Classification'):
     sc.main()
@@ -17,3 +18,5 @@ elif(rad=='Titanic Dataset'):
     td.main() 
 elif(rad=='Obesity Classification'):
     oc.main()
+elif(rad=='Flood Probability'):
+    fp.main()
